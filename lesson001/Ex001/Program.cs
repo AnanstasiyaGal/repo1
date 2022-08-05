714d80b1b2c51215412939a1951514797145c1fd
«Anastasiya» <Galieva.anastasiya@icloud.com>
2022-08-05 17:27:42 +0300

    ex001(ostatok ot deleniya)

---
+++ lesson001/Ex001/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 456;
+int b = 782;
+int c = 918;
+int result1 = a % 10;
+int result2 = b % 10;
+int result3 = c % 10;
+Console.WriteLine(result1);
+Console.WriteLine(result2);
+Console.WriteLine(result3);
