5638fea59c59c7525ed57154dc8aa343a03bb9ef
«Anastasiya» <Galieva.anastasiya@icloud.com>
2022-08-06 17:47:44 +0300

    Ex003 dni nedeli

---
+++ lesson001/Ex003/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Какой по счету сегодня день недели? Введите число от 1 до 7: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (Number == 2)
+{
+    Console.WriteLine("Втоник");
+}
+if (Number == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (Number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (Number == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (Number == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (Number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
